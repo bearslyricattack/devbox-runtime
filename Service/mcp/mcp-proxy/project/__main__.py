bc743e8045a5cdcade3394e9da1a397f231368b3
抒情熊 <2669184984@qq.com>
2025-04-23 15:26:57 +0800

    add mcp proxy.

--- Service/mcp/mcp-proxy/project/__main__.py
+++ Service/mcp/mcp-proxy/project/__main__.py
@@ -15,8 +15,8 @@ import typing as t
 
 from mcp.client.stdio import StdioServerParameters
 
-from .sse_client import run_sse_client
-from .sse_server import SseServerSettings, run_sse_server
+from sse_client import run_sse_client
+from sse_server import SseServerSettings, run_sse_server
 
 SSE_URL: t.Final[str | None] = os.getenv(
     "SSE_URL",
