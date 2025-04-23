58f2bb459b6b7e82c1255af12ba0e550249fdc8c
抒情熊 <2669184984@qq.com>
2025-04-23 15:50:01 +0800

    add mcp proxy.

--- Service/mcp/mcp-proxy/project/entrypoint.sh
+++ Service/mcp/mcp-proxy/project/entrypoint.sh
@@ -10,14 +10,16 @@ app_env=${1:-development}
 dev_commands() {
     echo "Running development environment commands..."
     # In the development environment, we may need more debugging information
-    python __main__.py --sse-port=8080 npx @modelcontextprotocol/server-puppeteer
+    cd /home/devbox
+    python -m project --sse-port=8080 npx @modelcontextprotocol/server-puppeteer
 }
 
 # Production environment commands
 prod_commands() {
     echo "Running production environment commands..."
     # In the production environment, we may need to add other parameters
-    python __main__.py --sse-port=8080 npx @modelcontextprotocol/server-puppeteer
+    cd /home/devbox
+    python -m project --sse-port=8080 npx @modelcontextprotocol/server-puppeteer
 }
 
 # Check environment variables to determine the running environment
